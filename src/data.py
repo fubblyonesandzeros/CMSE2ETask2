7ffd4566330f9dd123d4a82287fa341d37525830
Daniel Ik <codingeinstein@gmail.com>
2024-03-29 16:25:17 +0000

    initial ocmmit

---
+++ src/data.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class QuarkGluonClassificationDataset(Dataset):
+    def __init__(self, x_file, y_file):
+        self.x = np.load(x_file)
+        self.y = np.load(y_file)
+
+        self.min = -6.39
+        self.max = 16189.07
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, index):
+        x = self.x[index]
+        y = self.y[index]
+
+        x = torch.from_numpy(x).permute(2, 1, 0)
+        x = (x - self.min)/(self.max - self.min)
+
+        return x, y
---
+++ src/models.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+
+
+class VGGBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout, type="2"):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.dropout = dropout
+
+        layers = [
+            nn.Sequential(
+                nn.Conv2d(
+                    in_channels=in_channels, 
+                    out_channels=out_channels, 
+                    kernel_size=3, 
+                    stride=1,
+                    padding=1
+                ),
+                nn.Dropout2d(dropout),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU()
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(
+                    in_channels=out_channels, 
+                    out_channels=out_channels, 
+                    kernel_size=3, 
+                    stride=1,
+                    padding=1
+                ),
+                nn.Dropout2d(dropout),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU()
+
+            )
+        ]
+
+        if type == "3":
+            layers.append(
+                nn.Sequential(
+                    nn.Conv2d(
+                        in_channels=out_channels, 
+                        out_channels=out_channels, 
+                        kernel_size=3, 
+                        stride=1,
+                        padding=1
+                    ),
+                    nn.Dropout2d(dropout),
+                    nn.BatchNorm2d(out_channels),
+                    nn.ReLU()
+
+                )
+            )
+
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layers = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+
+
+class VGG(nn.Module):
+    def __init__(self, in_channels, dropout, classes):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            VGGBlock(in_channels=in_channels, out_channels=64, dropout=dropout, type="2"),
+            VGGBlock(in_channels=64, out_channels=128, dropout=dropout, type="2"),
+            VGGBlock(in_channels=128, out_channels=256, dropout=dropout, type="2"),
+            VGGBlock(in_channels=128, out_channels=256, dropout=dropout, type="3"),
+            VGGBlock(in_channels=256, out_channels=512, dropout=dropout, type="3"),
+            VGGBlock(in_channels=512, out_channels=512, dropout=dropout, type="3"),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(7*7*512, 4096),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+
+            nn.Linear(4096, 4096),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+
+            nn.Linear(4096, classes)
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.view(x.shape[0], -1)
+
+        x = self.fc(x)
+        return x
